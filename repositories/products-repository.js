b78cb15dc5a4e9d975e75a1aafcbc3aa50034a49
Ivan Fernandez <ivan.ferndz94@gmail.com>
2021-06-02 18:48:15 +0200

    Arreglo productos buggeados

--- repositories/products-repository.js
+++ repositories/products-repository.js
@@ -66,7 +66,8 @@ const getCatalogue = async (querySearch) => {
         );
         finalSearch = products;
     } else {
-        const getCatalogueQuery = 'SELECT * FROM products';
+        const getCatalogueQuery =
+            'SELECT products.id, products.seller_id, users.firstname AS seller, products.name, products.status, products.product_type, products.price, products.sale_status, products.location, products.description, sub_categories.name AS Subcategoria  FROM products  INNER JOIN products_has_subcategory ON products.id = products_has_subcategory.product_id INNER JOIN sub_categories ON products_has_subcategory.subcategory_id = sub_categories.id  INNER JOIN categories ON sub_categories.category_id = categories.id INNER JOIN users ON products.seller_id = users.id';
         const [products] = await database.pool.query(getCatalogueQuery);
         finalSearch = products;
     }
